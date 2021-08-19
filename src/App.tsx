c55c4732f9f4f625ea86af769b5006fc0efd2275
asmsuechan <suenagaryoutaabc@gmail.com>
2021-08-19 09:54:13 +0900

    Improve default markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react'
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 import { Preview } from './Preview'
 
-const DEFAULT_MD = '|left|center|right|\n|:-|:-:|-:|\n|**left**|[center](https://example.com)|right|'
+const DEFAULT_MD = '# h1\n## h2\n### h3\n#### h4\n\n## Texts\n**bold**\n\n__italic__\n\n~~strike~~\n\n[asmsuechan.com](https://asmsuechan.com)\n\n![profpic](https://avatars2.githubusercontent.com/u/11307908)\n\n## List\n* **bold**\n  * __nested__\n  * ~~nested~~\n* indent level 0\n\n+ plus mark\n  + nested\n\n- minus mark\n  - nested\n\n1. a\n  2. b**bold**\n\n## Table\n|left|center|right|\n|:-|:-:|-:|\n|**left**|[center](https://example.com)|right|\n\n## Blockquote\n> quote\n>> nested quote\n\n## Code\n```\nconsole.log(\'Hello, world!\')\n```'
 
 function App() {
   const [md, setMd] = useState((DEFAULT_MD))
